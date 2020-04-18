90151050bcf3828bb1b29f7dd7d1b9a91b7384c7
Jose.Fajardo <jose.fajardo@oracle.com>
2020-04-18 11:34:08 +1000

    moving in x , y direction for player

--- src/sandbox01/Assets/Scripts/TurretMovement.cs
+++ src/sandbox01/Assets/Scripts/TurretMovement.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 using static UnityEngine.InputSystem.InputAction;
 
 namespace DapperDino.BuildingBlocks
@@ -10,10 +12,26 @@ namespace DapperDino.BuildingBlocks
 
         private float previousInput;
 
+        private bool isX;
+        private bool isY;
+        private bool isZ;
+
         private void Update() => Move();
 
         public void MoveInput(CallbackContext ctx)
         {
+            if (ctx.control is KeyControl) {
+                var key = (KeyControl)ctx.control;
+                isX = isY = isZ = false;
+                switch (key.keyCode) {
+                    case Key.W:
+                    case Key.S:
+                        isY = true;
+                        break;
+                    default: isX = true; break;
+                }
+            }
+            
             if (ctx.performed)
             {
                 previousInput = ctx.ReadValue<float>();
@@ -29,9 +47,16 @@ namespace DapperDino.BuildingBlocks
         private void Move()
         {
             float movement = previousInput * movementSpeed * Time.deltaTime;
-            float newXValue = Mathf.Clamp(transform.position.x + movement, limits.x, limits.y);
 
-            transform.position = new Vector3(newXValue, transform.position.y, transform.position.z);
+            
+            if (isY) {
+                float newYValue = Mathf.Clamp(transform.position.y + movement, limits.x, limits.y);
+                transform.position = new Vector3(transform.position.x, newYValue, transform.position.z);
+            }
+            else { //isX
+                float newXValue = Mathf.Clamp(transform.position.x + movement, limits.x, limits.y);
+                transform.position = new Vector3(newXValue, transform.position.y, transform.position.z);
+            }
         }
     }
 }
