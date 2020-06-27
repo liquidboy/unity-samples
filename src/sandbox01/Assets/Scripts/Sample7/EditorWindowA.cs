be5c210109a6a03528d38acfd292f00d366bad48
Jose.Fajardo <jose.fajardo@oracle.com>
2020-06-27 11:15:48 +1000

    Sample7 : UIElements

---
+++ src/sandbox01/Assets/Scripts/Sample7/EditorWindowA.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EditorWindowA : EditorWindow
+{
+    [MenuItem("Window/My Window")]
+    public static void ShowWindow()
+    {
+        EditorWindow w = EditorWindow.GetWindow(typeof(EditorWindowA));
+
+        VisualTreeAsset uiAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Scripts/Sample7/EditorWindowA.uxml");
+        VisualElement ui = uiAsset.CloneTree(string.Empty);
+
+        w.rootVisualElement.Add(ui);
+    }
+
+    void OnGUI()
+    {
+        // Nothing to do here, unless you need to also handle IMGUI stuff.
+    }
+}
