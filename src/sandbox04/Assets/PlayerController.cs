5cfdde46763817a34723091e8e91dce8c067eaff
Jose.Fajardo <jose.fajardo@oracle.com>
2020-07-04 14:07:34 +1000

    sb4 : new navmes sample using old pipeline

---
+++ src/sandbox04/Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
